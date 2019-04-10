eb0a685954a445e19aab61d3661da87c233ed5a2
alessiamarcolini <98marcolini@gmail.com>
2019-04-10 11:57:10 +0200

    Get response on first dataset images

--- Utils/paralleldots_classification.py
+++ Utils/paralleldots_classification.py
@@ -1,12 +1,21 @@
+import os
 import requests
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
-multipart_data = MultipartEncoder(
-  fields={ 'file': ('test4.jpeg', open('test4.jpeg', 'rb'), 'image/jpeg')}
-)
+DATASET_DIR = '../../myq_dataset/Selfie-dataset/images/'
 
-response = requests.post( 'https://www.paralleldots.com/visual/facial/emotion',
-                         data=multipart_data,
-                         headers={'Content-Type': multipart_data.content_type})
+if os.path.exists(os.path.join(DATASET_DIR, '.DS_Store')):
+    os.remove(os.path.join(DATASET_DIR, '.DS_Store'))
 
-print(response.text)
+images_filenames = os.listdir(DATASET_DIR)
+
+for i, filename in enumerate(images_filenames):
+    filename_absolute = os.path.join(DATASET_DIR, filename)
+    #print(filename_absolute)
+    multipart_data = MultipartEncoder(fields={ 'file': (filename, open(filename_absolute, 'rb'), 'image/jpg')})
+    
+    response = requests.post('https://www.paralleldots.com/visual/facial/emotion', data=multipart_data, headers={'Content-Type': multipart_data.content_type})
+    print(response.text)
+    print(filename)
+    if i == 20:
+        break
